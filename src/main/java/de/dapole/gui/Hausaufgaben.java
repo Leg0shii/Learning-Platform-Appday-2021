e1d1da02bfe76981b50eda30ae8de033d1909f79
Power <joost.rehder@uni-rostock.de>
2021-05-28 20:31:10 +0200

    hausaufgaben oberflaeche

--- src/main/java/de/dapole/gui/Hausaufgaben.java
+++ src/main/java/de/dapole/gui/Hausaufgaben.java
@@ -1,4 +1,10 @@
 package de.dapole.gui;
 
+import javax.swing.*;
+
 public class Hausaufgaben {
+    private JButton homeButton;
+    private JButton ownButton;
+    private JButton sendHelpButton;
+    private JButton getHelpButton;
 }
