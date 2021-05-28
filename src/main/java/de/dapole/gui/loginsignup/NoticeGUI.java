e20074730e0ee631a0a555a24fe1bb238dd696b3
Power <joost.rehder@uni-rostock.de>
2021-05-29 00:36:42 +0200

    signed in notice

---
+++ src/main/java/de/dapole/gui/loginsignup/NoticeGUI.java
@@ -0,0 +1,35 @@
+package de.dapole.gui.loginsignup;
+
+import de.dapole.gui.GUI;
+import de.dapole.gui.GUIManager;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class NoticeGUI extends GUI {
+    private JButton toOverviewButton;
+    private JLabel noticeLabel;
+    private JPanel mainPanel;
+
+    public NoticeGUI(GUIManager guiManager) {
+        super(guiManager);
+        this.setLayout(new GridLayout(1, 1));
+        add(mainPanel);
+
+        setupGUI();
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        toOverviewButton.addActionListener(e -> toOverviewfunction());
+    }
+
+    private void toOverviewfunction() {
+        getGuiManager().switchToOverviewGUI();
+    }
+
+    private void setupGUI() {
+        this.toOverviewButton.setText("Weiter");
+        this.noticeLabel.setText("You have been signed in! Congratulations!");
+    }
+}
