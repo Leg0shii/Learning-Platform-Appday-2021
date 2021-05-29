36580a7e7c9e789b8cd9c1a8d8a848e802727fbd
Power <joost.rehder@uni-rostock.de>
2021-05-29 11:22:20 +0200

    comments

--- src/main/java/de/dapole/util/Leveling.java
+++ src/main/java/de/dapole/util/Leveling.java
@@ -2,6 +2,7 @@ package de.dapole.util;
 
 public class Leveling {
 
+    //benoetigte EXP fuer lvl: 25, 50 , 100, 200, 400, 800, 1600, 3200, .....
     public static int calcNextLevelEXP(int previouslevel) {
         return (int) (25 * Math.pow(2, previouslevel));
     }
